24277ae1bff29308e61f7a0884a6c3818182a9fe
Deepak Kumar <i.deepak0808@gmail.com>
2022-06-06 09:05:15 +0530

    Code

---
+++ LeetCode/1470.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans;
+        int size=n+n;
+        for(int i=0;i<n;i++)
+        {
+                ans.push_back(nums[i]);
+                ans.push_back(nums[i+n]);
+            
+        }
+        return ans;
+   
+    }
+};
