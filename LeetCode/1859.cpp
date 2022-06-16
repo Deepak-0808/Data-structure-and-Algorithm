b7d08a446fef3147bafb082dc3c104c3dfa8055b
Deepak Kumar <i.deepak0808@gmail.com>
2022-06-16 21:47:51 +0530

    Code

---
+++ LeetCode/1859.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        vector<string>pos(10,"");
+        for(int i=0; i<s.size(); i++){
+            int j = i;
+            string temp;
+            while(!isdigit(s[j])){
+                temp.push_back(s[j]);
+                j++;
+            }
+            pos[s[j]-'0'] = temp;
+            i = j+1;
+        }
+        string ans;
+        for(int i=1; i<10; i++){
+            if(pos[i].length()>0){
+                ans+=pos[i];
+                ans.push_back(' ');
+            }
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
