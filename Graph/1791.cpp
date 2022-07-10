d65e1af78b9cdacfea1b9c5550fd05c5d40df90d
Deepak Kumar <i.deepak0808@gmail.com>
2022-07-10 06:45:45 +0530

    Code

---
+++ Graph/1791.cpp
@@ -0,0 +1,14 @@
+//  1791. Find Center of Star Graph
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+       if(edges[0][0]==edges[1][0])
+            return edges[0][0];
+        else if(edges[0][0]==edges[1][1])
+            return edges[0][0];
+        else
+            return edges[0][1];
+        
+    }
+};
